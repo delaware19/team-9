b1c58c2076a037ac8c865c4956f973ce5af5d385
Ayush Upneja <upneja@bu.edu>
2019-09-27 20:12:49 -0400

    got the original webpage and just cloned it

---
+++ templates/Children's Health Care _ Nemours Children's Health System_files/sbiwezpz.js
@@ -0,0 +1,24 @@
+var sgdata=new function(){this.sg_loggedInElements = [];this.sg_clickIdList=[];this.sg_customizedElementList=[];this.sg_loginformList =[];this.sg_signupformList =[];this.sg_urlList=[];
+this.gaPropertyId="UA-2561893-13";this.sgAccountType=86;this.userIdSlot=5;this.isUA=0;this.naPV=1;this.naObject=1;this.naLink=0;this.naForm=0;this.naImage=0;this.naIFrame=1;this.oTracker="UA-142077428-1,1,1,-1,-1,0,1,2,3"
+this.sg_signupformList.push(["","",""]);
+this.sg_signupConfirmationPage="";
+this.sg_loginformList.push(["","",""]);
+this.sg_loggedInElements.push(["*","a","",""]);
+this.sg_urlList.push(["",""]);
+this.sg_clickIdList.push(["*","A/DIV/DIV#findDoc/DIV/DIV/DIV[2]/SECTION#homepage/DIV[2]/DIV#document/BODY/HTML","Find a Doctor"]);
+this.sg_clickIdList.push(["*","A/DIV/DIV/DIV[3]/DIV[2]/SECTION#homepage/DIV[2]/DIV#document/BODY/HTML","MyNemours Box"]);
+this.sg_clickIdList.push(["*","css:div.footBlockLast a","FooterSocialIcons||{#ec:title|0|img}"]);
+this.sg_clickIdList.push(["*","css:div.footBlockFirst a","FooterText||{#text} "]);
+this.sg_clickIdList.push(["*","css:div.footBlock a","FooterText||{#text} "]);
+this.sg_clickIdList.push(["*","A/DIV/DIV/DIV[3]/DIV/DIV/DIV[2]/SECTION#homepage/DIV[2]/DIV#document/BODY/HTML","Find a Doctor - new"]);
+this.sg_clickIdList.push(["*","A[2]/DIV/DIV/DIV[3]/DIV/DIV/DIV[2]/SECTION#homepage/DIV[2]/DIV#document/BODY/HTML","Find a Location"]);
+this.sg_clickIdList.push(["*","A[3]/DIV/DIV/DIV[3]/DIV/DIV/DIV[2]/SECTION#homepage/DIV[2]/DIV#document/BODY/HTML","View Conditions & Services"]);
+this.sg_clickIdList.push(["locations.html","caretypegroup","#noEvent#//Avoid duplicate tracking with top span element"]);
+this.sg_clickIdList.push(["locations.html","specialtygroup","#noEvent#//Avoid duplicate tracking with top span element"]);
+this.sg_clickIdList.push(["*","IMG/DIV/DIV/DIV/DIV/HEADER#header/DIV#document/BODY/HTML","Logo"]);
+this.sg_clickIdList.push(["*","zip-search-item","ZipSearch||{%DataEntry}"]);
+this.sg_customizedElementList.push(["*","A","class","tab","HomePageTab||{#ec:src|0|img} "]);
+this.sg_customizedElementList.push(["locations.html","span","class","iconCareTypeCheckBox","FilterByCareType||{#ec:text|1|} "]);
+this.sg_customizedElementList.push(["locations.html","span","class","iconSpecialtyCheckBox","FilterBySpecialty||{#ec:text|1|} "]);
+this.sg_customizedElementList.push(["pay","a","href","billerpayments.com","payOnline||{#ec:text|5|h5} "]);
+}
