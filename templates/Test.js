a12665c20a8377c5e740956d7cca62217a3a1999
Kevin Jerome <kdj5158@psu.edu>
2019-09-28 09:57:18 -0400

    NOW CAN READ FROM JSON

--- templates/Test.js
+++ templates/Test.js
@@ -5,10 +5,8 @@ function submit() {
     temp = {"image":document.getElementById("image").value, "text":document.getElementById("text").value};
     stories.push(temp);
 
-    var c = {};
-    c = stories;
     var story = {"id": 0,
-                "pages": c,
+                "pages": stories,
                 "age": document.getElementById("age").value,
                 "title": document.getElementById("title").value,
                 "isFavorited": false,
@@ -21,6 +19,18 @@ function doSomething() {
     var temp = {};
     temp = {"image":document.getElementById("image").value, "text":document.getElementById("text").value};
     stories.push(temp);
+}
+
+function drawToCanvas() {
+    //document.getElementById("title").value = SocialStories.getItem("title")
+    var obj = JSON.parse(SocialStories.getItem("Story"))
+    document.getElementById("title").setAttribute("value", obj.title);
+    document.getElementById("age").setAttribute("value", obj.age);
+    document.getElementById("gender").setAttribute("value", obj.gender);
+    document.getElementById("ethnicity").setAttribute("value", obj.ethnicity);
+    //obj.pages[0].text, where 0 represents the page number and text is that page's text
+    document.getElementById("text").setAttribute("value", obj.pages[0].text);
+
 }
 var newstory = document.getElementById("NewStory");
 newstory.addEventListener("submit", submit);
